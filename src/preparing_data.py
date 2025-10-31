0bec30cf60ecc376afcc4ba2f045641708b6482d
VadimBarinov <barinovvadim87@gmail.com>
2025-10-31 17:43:39 +0400

    refactor preparing_data.py: delete functions load_df and save_df_to_csv + rename train_test_split_ddf to train_validation_test_split_ddf + refactor config.py

--- src/preparing_data.py
+++ src/preparing_data.py
@@ -1,18 +1,16 @@
 from typing import Any
 import dask.dataframe as dd
-from dask_ml.model_selection import train_test_split
-import numpy as np
 import requests
-from pandas import pivot_table
 from pathlib import Path
 from zipfile import ZipFile
 
 from src.config import settings
 
+
 def download_csv(
         input_folder_path:str, 
         url: str):
-    
+    """Загрузка датасета"""
     input_folder = Path(input_folder_path)
     input_folder.mkdir(exist_ok=True, parents=True)
     zip_file = "dataset.zip"
@@ -37,15 +35,8 @@ def download_csv(
             print("All csv files was unziped")
         full_path.unlink()
 
-def load_df(
-        path_to_rating_csv: str,
-) -> dd.DataFrame:
-    rating = dd.read_csv(path_to_rating_csv)
 
-    return rating
-
-
-def train_test_split_ddf(
+def train_validation_test_split_ddf(
         data: dd.DataFrame,
         test_ratio: float = settings.data.test_size,
         validation_ratio: float = settings.data.validation_size
@@ -70,31 +61,10 @@ def train_test_split_ddf(
     return train, validation, test
 
 
-def save_df_to_csv(
-        data: dd.DataFrame,
-        path: str,
-) -> None:
-    data.to_csv(
-        path,
-        index=False,
-    )
-
-
 def get_user_movie_df(
         data: dd.DataFrame,
 ) -> dd.DataFrame:
-    # print(data.npartitions)
-    # for n_part in range(data.npartitions):
-    #     part= data.get_partition(n_part)
-    #     pivot_data = part.pivot_table(
-    #         index="userId",
-    #         columns="movieId",
-    #         values="rating",
-    #     ).fillna(0)
-    #     pivot_data.compute().to_csv(
-    #         f"{settings.data.csv_save_train_path}/{n_part}.part",
-    #     )
-    #     print(n_part)
+    """Построение user-movie матрицы"""
     data = data.categorize(columns=[
         settings.data.column_names.movieId
     ])
@@ -104,8 +74,5 @@ def get_user_movie_df(
         columns=settings.data.column_names.movieId,
         values=settings.data.column_names.rating,
     ).fillna(0)
-    # print(pivot_data.npartitions)
 
     return pivot_data
-
-
